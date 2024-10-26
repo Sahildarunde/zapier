e9324efb12bb62922d940486e1795dc8a78c1969
Sahil Darunde <sahildarunde007@gmail.com>
2024-10-26 13:00:33 +0530

    primary-backend hosted

--- frontend/app/zap/create/page.tsx
+++ frontend/app/zap/create/page.tsx
@@ -144,7 +144,7 @@ function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (
                         onSelect(null);
                     }} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="default-modal">
                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                         </svg>
                         <span className="sr-only">Close modal</span>
                     </button>
@@ -157,7 +157,7 @@ function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (
                         })
                     }} />}
 
-                    {(step === 1 && selectedAction?.id === "sol") && <SolanaSelector setMetadata={(metadata) => {
+                    {(step === 1 && selectedAction?.id === "send-sol") && <SolanaSelector setMetadata={(metadata) => {
                         onSelect({
                             ...selectedAction,
                             metadata
@@ -165,6 +165,8 @@ function Modal({ index, onSelect, availableItems }: { index: number, onSelect: (
                     }} />}
 
                     {step === 0 && <div>{availableItems.map(({id, name, image}) => {
+
+
                             return <div key={id} onClick={() => {
                                 if (isTrigger) {
                                     onSelect({
