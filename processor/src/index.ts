309024668b60283103940ad54de39067ecf15d99
Sahil Darunde <sahildarunde007@gmail.com>
2024-10-29 13:14:27 +0530

    endpoint in procesor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -55,9 +55,15 @@ main().catch(err => {
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Worker is running');
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ status: 'healthy' }));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
+
 
 server.listen(PORT, () => {
     console.log(`HTTP server running on port ${PORT}`);
